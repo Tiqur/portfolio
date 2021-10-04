d5570b0204b6e52671def5375a8d2d5cfaec4ed7
Tiqur <Tiqur7@gmail.com>
2021-10-04 03:06:05 -0700

    Added console message on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { Layout, Navbar } from './components/index';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    console.log('Designed and developed by Trevor Brage ( Github: https://github.com/Tiqur/ )')
+  }, [])
+
   return (
     <Layout>
       <Navbar>
