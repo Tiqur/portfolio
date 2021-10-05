f32bb02d317ccff66e0460204c8e46eefdc69018
Tiqur <Tiqur7@gmail.com>
2021-10-04 21:24:22 -0700

    Overlay mobile nav menu over root

--- src/components/navbar/main.tsx
+++ src/components/navbar/main.tsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.scss';
 import { MenuSvg } from '../../assets/index';
 import { Link } from 'react-router-dom';
+import usePortal from 'react-useportal';
 import { useState } from 'react';
 
 const NavLink: React.FC<{text:string, to:string}> = (props) => {
@@ -16,10 +17,23 @@ export const Navbar: React.FC = (props) => {
   // State for hamburger menu
   const [menuState, setMenuState] = useState(false);
 
+  // Craete portal to root for nav overlay
+  const { Portal } = usePortal({
+    bindTo: document.getElementById('root') as HTMLElement
+  })
+
   return (
     <div className={styles.container}>
       <header className={styles.navbar}>
         <MenuSvg className={styles.hamburger_menu} onClick={() => setMenuState(!menuState)}/>
+
+        { /* Conditionally render mobile_nav_menu if "menuState" */ }
+        { menuState && 
+          <Portal>
+            <div className={styles.mobile_nav_menu}/>
+          </Portal>
+        }
+
         <div className={styles.navlink_container}>
           <NavLink to='#' text='Home'/>
           <NavLink to='#' text='About Me'/>
