47549106dc69a32ee9bf1203022e5a54da23d963
Tiqur <Tiqur7@gmail.com>
2021-10-05 14:58:32 -0700

    Added basic mobile nav links

--- src/components/navbar/main.tsx
+++ src/components/navbar/main.tsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import usePortal from 'react-useportal';
 import { useState } from 'react';
 
-const NavLink: React.FC<{text:string, to:string}> = (props) => {
+const NavLink: React.FC<{text:string, to:string, className:string}> = (props) => {
   return (
     <Link to={props.to}>
-      <p className={styles.navlink_text}>{props.text}</p>
+      <p className={props.className}>{props.text}</p>
     </Link>
   )
 }
@@ -22,24 +22,32 @@ export const Navbar: React.FC = (props) => {
     bindTo: document.getElementById('root') as HTMLElement
   })
 
+  const nav_links = [
+    { to: '#', text: 'Home'},
+    { to: '#', text: 'About Me'},
+    { to: '#', text: 'Projects'},
+    { to: '#', text: 'Contact'}
+  ]
+
   return (
     <div className={styles.container}>
       <header className={styles.navbar}>
-        <MenuSvg className={styles.hamburger_menu} onClick={() => setMenuState(!menuState)}/>
 
+        {/* Mobile nav */}
+        <MenuSvg className={styles.hamburger_menu} onClick={() => setMenuState(!menuState)}/>
         <Portal>
-          <div onClick={() => setMenuState(!menuState)} className={`${styles.mobile_nav_menu} ${menuState ? styles.mobile_nav_menu_active : styles.mobile_nav_menu_hidden}`}/>
+          <div onClick={() => setMenuState(!menuState)} className={`${styles.mobile_nav_menu} ${menuState ? styles.mobile_nav_menu_active : styles.mobile_nav_menu_hidden}`}>
+            {nav_links.map((e) => <NavLink className={styles.navlink_mobile} to={e.to} text={e.text}/>)}
+          </div>
         </Portal>
 
+        {/* Desktop nav */}
         <div className={styles.navlink_container}>
-          <NavLink to='#' text='Home'/>
-          <NavLink to='#' text='About Me'/>
-          <NavLink to='#' text='Home'/>
+          {nav_links.map((e) => <NavLink className={styles.navlink_desktop} to={e.to} text={e.text}/>)}
         </div>
 
       </header>
       {props.children}
     </div>
-
   )
 }
