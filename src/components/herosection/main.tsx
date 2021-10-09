58167ba918e7d867dbee52642c9ca864a28c07af
Tiqur <Tiqur7@gmail.com>
2021-10-09 00:14:25 -0700

    Move HeroSection to seperate component

---
+++ src/components/herosection/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import { ScrollIndicator } from '../../components/index';
+
+
+export const HeroSection: React.FC = () => {
+  return (
+   <div className={styles.container}>
+     <div className={styles.hero}>
+       <h1 style={{color: '#EF008A', marginLeft: '0.08em', marginTop: '0.08em'}} className={styles.hero_text}>Trevor Brage</h1>
+       <h1 style={{color: '#3071E8', marginLeft: '0.05em', marginTop: '0.05em'}} className={styles.hero_text}>Trevor Brage</h1>
+       <h1 style={{color: '#FFFFFF'}} className={styles.hero_text}>Trevor Brage<h2 className={styles.hero_text2}>Full stack developer</h2></h1>
+     </div>
+     <ScrollIndicator className={styles.scroll_indicator}/>
+   </div>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export {Layout} from './layout/main';
 export {Navbar} from './navbar/main';
+export {HeroSection} from './herosection/main';
 export {ScrollIndicator} from './scrollindicator/main';
