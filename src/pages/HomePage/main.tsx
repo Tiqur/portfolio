bf3d40318df13cd1ffb7a116abe816f8f0907b7d
Tiqur <Tiqur7@gmail.com>
2021-10-08 21:17:53 -0700

    Added left hero alignment for wider viewports

--- src/pages/HomePage/main.tsx
+++ src/pages/HomePage/main.tsx
@@ -6,7 +6,7 @@ export const HomePage: React.FC = () => {
   return (
    <div className={styles.container}>
     <div className={styles.hero}>
-      <h1 style={{color: '#EF008A', marginLeft: '0.1em', marginTop: '0.1em'}} className={styles.hero_text}>Trevor Brage</h1>
+      <h1 style={{color: '#EF008A', marginLeft: '0.08em', marginTop: '0.08em'}} className={styles.hero_text}>Trevor Brage</h1>
       <h1 style={{color: '#3071E8', marginLeft: '0.05em', marginTop: '0.05em'}} className={styles.hero_text}>Trevor Brage</h1>
       <h1 style={{color: '#B8B8B8'}} className={styles.hero_text}>Trevor Brage<h2 className={styles.hero_text2}>Full stack developer</h2></h1>
     </div>
