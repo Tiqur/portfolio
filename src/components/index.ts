cbfa0cef444b8455ec9e0b577ff32117d1538b87
Tiqur <Tiqur7@gmail.com>
2021-10-09 02:05:16 -0700

    Initialize about section

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export {Layout} from './layout/main';
 export {Navbar} from './navbar/main';
 export {HeroSection} from './herosection/main';
+export {AboutSection} from './aboutsection/main';
 export {ScrollIndicator} from './scrollindicator/main';
--- src/pages/HomePage/main.tsx
+++ src/pages/HomePage/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import { HeroSection } from '../../components/index';
+import { HeroSection, AboutSection } from '../../components/index';
 
 
 export const HomePage: React.FC = () => {
   return (
    <div className={styles.container}>
     <HeroSection/>
+    <AboutSection/>
    </div>
   )
 }
