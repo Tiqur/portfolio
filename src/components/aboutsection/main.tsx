b7197ae7fa0e18d6f35bb06b7f531bb99ce4639d
Tiqur <Tiqur7@gmail.com>
2021-10-09 02:14:31 -0700

    Correctly position about section background

--- src/components/aboutsection/main.tsx
+++ src/components/aboutsection/main.tsx
@@ -5,7 +5,8 @@ import styles from './styles.module.scss';
 export const AboutSection: React.FC = () => {
   return (
    <div className={styles.container}>
-
+    <div className={styles.background}/>
+    <div className={styles.inner}/>
    </div>
   )
 }
