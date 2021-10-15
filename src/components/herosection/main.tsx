b10f8b646324bbef0e6094fd4c1d02da9f749a04
Tiqur <Tiqur7@gmail.com>
2021-10-14 23:29:06 -0700

    Fixed nested element warning

--- src/components/herosection/main.tsx
+++ src/components/herosection/main.tsx
@@ -9,7 +9,7 @@ export const HeroSection: React.FC = () => {
      <div className={styles.hero}>
        <h1 style={{color: '#EF008A', marginLeft: '0.08em', marginTop: '0.08em'}} className={styles.hero_text}>Trevor Brage</h1>
        <h1 style={{color: '#3071E8', marginLeft: '0.05em', marginTop: '0.05em'}} className={styles.hero_text}>Trevor Brage</h1>
-       <h1 style={{color: '#FFFFFF'}} className={styles.hero_text}>Trevor Brage<h2 className={styles.hero_text2}>Full stack developer</h2></h1>
+       <h1 style={{color: '#FFFFFF'}} className={styles.hero_text}>Trevor Brage<div className={styles.hero_text2}>Full stack developer</div></h1>
      </div>
      <ScrollIndicator className={styles.scroll_indicator}/>
    </div>
