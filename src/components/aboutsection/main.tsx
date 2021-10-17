5b403d96ca6360c29b6780cd4a00b30b0eb79533
Tiqur <Tiqur7@gmail.com>
2021-10-17 00:11:38 -0700

    Added inner section styling and content

--- src/components/aboutsection/main.tsx
+++ src/components/aboutsection/main.tsx
@@ -4,11 +4,11 @@ import { About1Svg } from '../../assets/index'
 import { useRef, useState, useEffect } from 'react';
 
 
-const InnerSection: React.FC = (props) => {
+const InnerSection: React.FC<{text: string}> = (props) => {
   return (
-    <div>
+    <div className={styles.inner_section}>
       <About1Svg/>
-      <p>1</p>
+      <p className={styles.inner_section_text}>{props.text}</p>
     </div>
   )
 }
@@ -39,9 +39,9 @@ export const AboutSection: React.FC = () => {
     <p ref={titleRef} className={styles.title}>About Me</p>
     <p ref={textRef} className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna, turpis elementum pulvinar. Neque ultrices nulla auctor quam curabitur suscipit. Elementum libero tempus interdum nulla gravida euismod amet nullam tortor. Odio erat suspendisse faucibus neque, pretium velit. Dapibus laoreet nam fermentum nascetur euismod duis. Pulvinar vestibulum pellentesque mattis consequat eget tincidunt tristique feugiat vel. Habitant tincidunt justo tincidunt sit semper condimentum. Mauris mi metus, eget libero. Ac elit et interdum.</p>
     <div ref={innerContainerRef} className={styles.inner}>
-      <InnerSection/>
-      <InnerSection/>
-      <InnerSection/>
+      <InnerSection text='gravida euismod amet nullam tortor. Odio erat suspe'/>
+      <InnerSection text='gravida euismod amet nullam tortor. Odio erat suspe'/>
+      <InnerSection text='gravida euismod amet nullam tortor. Odio erat suspe'/>
     </div>
    </div>
   )
