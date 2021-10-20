038c23b25975d3055fd0c341c4469b6a1ff10301
Tiqur <Tiqur7@gmail.com>
2021-10-20 00:38:32 -0700

    Added carousel navigation buttons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,4 @@ export {ReactComponent as HomeSvg} from './home_white_24dp.svg';
 export {ReactComponent as PersonSvg} from './person_white_24dp.svg';
 export {ReactComponent as BriefcaseSvg} from './cases_white_24dp.svg';
 export {ReactComponent as MessageSvg} from './message_white_24dp.svg';
+export {ReactComponent as ArrowForwardSvg} from './arrow_forward_ios.svg';
--- src/components/carouselsection/main.tsx
+++ src/components/carouselsection/main.tsx
@@ -1,13 +1,14 @@
 import styles from './styles.module.scss';
+import { ArrowForwardSvg } from '../../assets/index';
 
 export const CarouselSection = () => {
   return (
     <div className={styles.container}>
       <p className={styles.title}>Projects</p>
       <div className={styles.inner_container}>
-        <div className={styles.button}/>
+        <ArrowForwardSvg className={styles.c_buttonL}/>
         <div className={styles.content} />
-        <div className={styles.button}/>
+        <ArrowForwardSvg className={styles.c_button}/>
       </div>
       <div className={styles.navigation}>
       </div>
