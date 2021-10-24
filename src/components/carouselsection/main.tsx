5fd2535c89153f81dcccc25a734536f440c4111b
Tiqur <Tiqur7@gmail.com>
2021-10-23 22:33:06 -0700

    Implemented pagination click event

--- src/components/carouselsection/main.tsx
+++ src/components/carouselsection/main.tsx
@@ -1,19 +1,74 @@
 import styles from './styles.module.scss';
 import { ArrowForwardSvg } from '../../assets/index';
+import React, { useState } from 'react';
 
 export const CarouselSection = () => {
+  const projects = [
+    {
+      name: "Project1",
+      image: 'image',
+      description: "Project1 Description",
+      icons: ['scss', 'react', 'js', 'html'],
+      github: 'https://github.com',
+      website: 'https://google.com', // If this matches website url, render "Demo" button
+    },
+    {
+      name: "Project2",
+      image: 'image',
+      description: "Project2 Description",
+      icons: ['scss', 'react', 'js', 'html'],
+      github: 'https://github.com',
+      website: 'https://google.com'
+    },
+    {
+      name: "Project2",
+      image: 'image',
+      description: "Project2 Description",
+      icons: ['scss', 'react', 'js', 'html'],
+      github: 'https://github.com',
+      website: 'https://google.com'
+    },
+    {
+      name: "Project3",
+      image: 'image',
+      description: "Project3 Description",
+      icons: ['scss', 'react', 'js', 'html'],
+      github: 'https://github.com',
+      website: 'https://google.com'
+    },
+    {
+      name: "Project4",
+      image: 'image',
+      description: "Project4 Description",
+      icons: ['scss', 'react', 'js', 'html'],
+      github: 'https://github.com',
+      website: 'https://google.com'
+    }
+  ]
+
+
+  let [project_index, set_project_index] = useState(0);
+
+  const handle_pagination = (direction: string) => {
+    if (direction === 'right') {
+      set_project_index(project_index === projects.length-1 ? 0 : project_index + 1);
+    } else {
+      set_project_index(project_index === 0 ? projects.length-1 : project_index - 1);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <p className={styles.title}>Projects</p>
       <div className={styles.inner_container}>
-        <ArrowForwardSvg className={styles.c_buttonL}/>
+        <ArrowForwardSvg onClick={() => handle_pagination('left')} className={styles.c_buttonL}/>
         <div className={styles.content} />
-        <ArrowForwardSvg className={styles.c_button}/>
+        <ArrowForwardSvg onClick={() => handle_pagination('right')} className={styles.c_button}/>
       </div>
       <div className={styles.navigation}>
       </div>
       <div className={styles.pagination}>
-        <ArrowForwardSvg className={styles.p_buttonL}/>
+        <ArrowForwardSvg onClick={() => handle_pagination('left')} className={styles.p_buttonL}/>
         <div className={styles.p_dot}/>
         <div className={styles.p_dot}/>
         <div className={styles.p_dot}/>
@@ -21,7 +76,7 @@ export const CarouselSection = () => {
         <div className={styles.p_dot}/>
         <div className={styles.p_dot}/>
         <div className={styles.p_dot}/>
-        <ArrowForwardSvg className={styles.p_button}/>
+        <ArrowForwardSvg onClick={() => handle_pagination('right')} className={styles.p_button}/>
       </div>
     </div>
   )
